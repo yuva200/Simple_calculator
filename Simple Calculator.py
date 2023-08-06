aa0cd24289206ff0b9019588a251716abf7c95bb
Ratnala Yuvaraju <129956949+yuva200@users.noreply.github.com>
2023-08-06 20:19:36 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,17 @@
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Choose an operation (+, -, *, /): ")
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        result = num1 / num2
+    else:
+        print("Invalid operation selected.")
+        return
+    print(" The result is: ", result)
+calculator()
